f46735af77c275c70358c2262f7481541e946d10
Nikita Khromochkin <129375503+Derevoed@users.noreply.github.com>
2023-06-19 02:24:26 +1100

    Add file - "test"

---
+++ Module_24/Task_24.2.4/tests/test.py
@@ -0,0 +1,26 @@
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply(self):
+        assert self.calc.multiply(self, 5, 0) == 0
+
+    def test_division(self):
+        assert self.calc.division(self, 8, 2) == 4
+
+    def test_subtraction(self):
+        assert self.calc.subtraction(self, 7, 2) == 5
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 9, 1) == 10
+
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self, 1, 0)
+
+    def teardown(self):
+        print('Выполнение метода Teardown')

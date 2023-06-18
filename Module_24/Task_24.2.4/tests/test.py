9b6c4c7690593f467008a31c4c582f510b4502f9
Nikita_Khromochkin <DDerevoed@yandex.ru>
2023-06-19 02:47:38 +1100

    Update the file

--- Module_24/Task_24.2.4/tests/test.py
+++ Module_24/Task_24.2.4/tests/test.py
@@ -18,9 +18,5 @@ class TestCalc:
     def test_adding_success(self):
         assert self.calc.adding(self, 9, 1) == 10
 
-    def test_zero_division(self):
-        with pytest.raises(ZeroDivisionError):
-            self.calc.division(self, 1, 0)
-
     def teardown(self):
         print('Выполнение метода Teardown')
